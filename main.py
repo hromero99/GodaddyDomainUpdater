5ad744eb06ccf226426f3eca64bd7784600726f2
hromero99 <hromero@protonmail.com>
2020-09-27 21:45:41 +0200

    Added parameters to script

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-import os 
 import requests
 import logging
 from api import GodaddyApi
@@ -18,14 +17,15 @@ def get_current_local_ip() -> str:
 
 if __name__ == "__main__":
     load_dotenv()
-    domain = os.environ.get("DOMAIN")
     api_controller = GodaddyApi()
     parser = argparse.ArgumentParser(description="Update information about domain in Goddady web")
-    parser.add_argument(dest="domain", type=str, help="Domain to perform operations")
-    parser.add_argument(dest="ip", type=str, help="Ip Address to update information of DNS A register")
-    parser.add_argument(dest="subdomain", type=str, help="Ip Address to update information of DNS A register")
-    parser.add_argument("-c", "--createSubdomain", help="Create given domain if not exits", action='store_true')
-    parser.add_argument("-u", "--update", help="Update A register of given Domain to given Ip", action='store_true')
+    required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
+    optional.add_argument("-c", "--createSubdomain", help="Create given domain if not exits", action='store_true')
+    optional.add_argument("-u", "--update", help="Update A register of given Domain to given Ip", action='store_true')
+    required.add_argument("--domain", action="store", help="Domain to perform operations", required=True)
+    required.add_argument("--ip", action="store", help="Ip Address to update information of DNS A register", required=True)
+    optional.add_argument("--subdomain", action="store", help="Ip Address to update information of DNS A register")
 
     arguments = parser.parse_args()
 
@@ -34,5 +34,10 @@ if __name__ == "__main__":
             domain=arguments.domain,
             subdomain=arguments.subdomain,
             ip=arguments.ip
+        )
+    if arguments.update:
+        api_controller.update_godaddy_a_register(
+            ip=arguments.ip,
+            domain=arguments.domain
         )
 
