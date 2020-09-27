7d5d281caacaee15f8b68a873750d6557d7f606a
hromero99 <hromero@protonmail.com>
2020-09-27 14:27:35 +0200

    Add arguments and function call for create subdomain

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 import requests
 import logging
 from api import GodaddyApi
+import argparse
 
-logger = logging.basicConfig(level=logging.INFO,filename="domainUpdater.log")
+logging.basicConfig(level=logging.INFO, filename="domainUpdater.log")
 
 
 def get_current_local_ip() -> str:
@@ -17,17 +18,21 @@ def get_current_local_ip() -> str:
 
 if __name__ == "__main__":
     load_dotenv()
-    ip = get_current_local_ip()
     domain = os.environ.get("DOMAIN")
     api_controller = GodaddyApi()
-    if (os.environ.get("KEY") == "") or (os.environ.get("SECRET") == "") or (domain == ""):
-        print("You must set SECRET, KEY and DOMAIN env vars")
-        exit(-1)
-    if ip:
-        ipChanged = api_controller.verify_change_ip(ip, domain)
-        if ipChanged == 0:
-            api_controller.update_godaddy_a_register(ip, domain)
-        elif ipChanged == -1:
-            logging.info(f"Domain hromero99.club already has ip {ip}")
-        else:
-            logging.error(f"UnkownError {ipChanged}")
+    parser = argparse.ArgumentParser(description="Update information about domain in Goddady web")
+    parser.add_argument(dest="domain", type=str, help="Domain to perform operations")
+    parser.add_argument(dest="ip", type=str, help="Ip Address to update information of DNS A register")
+    parser.add_argument(dest="subdomain", type=str, help="Ip Address to update information of DNS A register")
+    parser.add_argument("-c", "--createSubdomain", help="Create given domain if not exits", action='store_true')
+    parser.add_argument("-u", "--update", help="Update A register of given Domain to given Ip", action='store_true')
+
+    arguments = parser.parse_args()
+
+    if arguments.createSubdomain:
+        api_controller.create_subdomain(
+            domain=arguments.domain,
+            subdomain=arguments.subdomain,
+            ip=arguments.ip
+        )
+
